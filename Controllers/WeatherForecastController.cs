ce56ebf2a428403214f8868dd60170710d4702ed
AKILANDESWARIGOVINDASAMY <akila.g5@tcs.com>
2020-09-23 17:16:08 +0530

    Add project files.

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace APIGateway.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var msg = Assembly.GetExecutingAssembly().GetName().Name + " is up & running AKILA!";
+            return StatusCode(StatusCodes.Status200OK, msg);
+        }
+    }
+}
